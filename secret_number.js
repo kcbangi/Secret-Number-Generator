78843b80add583aea231911d173035b9cf3c7fc0
Keith <kayzee@live.com>
2019-07-03 13:33:01 -1000

    passed tests

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,11 @@
 'use strict';
 module.exports = function() {
-  
+    
+    const randomNumberGenerator = Math.floor(Math.random() * 1000000);
+
+    function secretNumber() {
+        return randomNumberGenerator;
+    };
+    return secretNumber;
+
 };

8d950ebc905f2fc2a8a01d0bc55c6c51520af8d0
Keith <kayzee@live.com>
2019-07-11 21:48:45 -1000

    pr

--- secret_number.js
+++ secret_number.js
@@ -1,13 +1,10 @@
-'use strict';
+"use strict";
 module.exports = function() {
-    
-    const randomNumberGenerator = Math.floor(Math.random() * 1000000);
+  const randomNumberGenerator = Math.floor(Math.random() * 1000000);
 
-    function secretNumber() {
-        return randomNumberGenerator;
-    };
+  function secretNumber() {
+    return randomNumberGenerator;
+  }
 
-    return secretNumber;
-      
+  return secretNumber;
 };
-
